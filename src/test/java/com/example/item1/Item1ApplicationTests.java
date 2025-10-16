04fc3a4b65b705b4fb3b9b19057d56d7024c2bf9
lsb06111 <soobinlee647@gmail.com>
2025-10-16 16:42:38 +0900

    first commit

---
+++ src/test/java/com/example/item1/Item1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.item1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Item1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
