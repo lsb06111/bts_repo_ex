7ea7a5ac6732706d79905eec68d77de050934552
lsb06111 <soobinlee647@gmail.com>
2025-11-12 11:51:05 +0900

    컴파일 에러

--- src/main/java/com/example/item1/Item1Application.java
+++ src/main/java/com/example/item1/Item1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Item1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Item1Application.class, args);
+		SpringApplication.run(Item1Application.class, arg);
 	}
 
 }
